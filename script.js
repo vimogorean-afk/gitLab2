c0209c9fc46c7262a547721d71dbfe525921e74a
vimogorean-afk <vimogorean@gmail.com>
2026-05-08 12:33:46 +0300

    Partea doi

--- script.js
+++ script.js
@@ -17,5 +17,30 @@ class toDoList {
             this.createTodoElements(todo.id, todo.todoText);   
         }
     }
+     bindEvents(){
+        this.form.addEventListener("submit", this.onSubmit.bind(this));   
+        this.todoContent.addEventListener("click", this.onClick.bind(this));  
+    }
+    createTodoElements(id, text){
+        const todoDiv = document.createElement('div');
+        todoDiv.classList.add('is-flex');
+        todoDiv.setAttribute('id', id);
 
+        const todoText = document.createElement('h3');
+        todoText.textContent = text;
+
+        const todoDelete = document.createElement('button');
+        todoDelete.textContent = 'X';
+        todoDelete.setAttribute('id','delete');
+
+        const todoEdit = document.createElement('button');
+        todoEdit.textContent = 'Edit';
+        todoEdit.setAttribute('id','edit');
+
+        todoDiv.appendChild(todoText);
+        todoDiv.appendChild(todoDelete);
+        todoDiv.appendChild(todoEdit);
+
+        this.todoContent.appendChild(todoDiv);
+    }
 }
