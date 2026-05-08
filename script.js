85bab561d14ef4c98b240487dcafd33ef8d31e0b
vimogorean-afk <vimogorean@gmail.com>
2026-05-08 12:28:35 +0300

    clas toDo

---
+++ script.js
@@ -0,0 +1,21 @@
+class TodoApp {
+    constructor(){
+        
+        this.form = document.querySelector('form'); 
+        this.todoInput = document.querySelector('#todo-input');
+        this.todoContent = document.querySelector('#todo-content');
+
+        
+        this.todosArr = JSON.parse(localStorage.getItem('todosDB')) ?? [];
+
+         
+        this.loadInitialData(); 
+        this.bindEvents();   
+    }
+    loadInitialData(){   
+        for (const todo of this.todosArr){ 
+            this.createTodoElements(todo.id, todo.todoText);   
+        }
+    }
+
+}
